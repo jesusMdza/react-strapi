78bf378ae4f7871a41d950fe858e44bb4b8f5947
Jesus Mendoza <jesuszmendoza14@gmail.com>
2020-12-22 22:16:01 -0600

    feat(app): initialize strapi and react frontend

---
+++ strapi/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd80dba73646b5c19eef0530533594a7d'),
+    },
+  },
+});
---
+++ strapi/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0cf1111a-6449-450f-bc3f-700b85580d59'
+};
